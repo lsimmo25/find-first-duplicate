7112824c4789cfe0629c0e2b55953419043ff257
Lyle Simmons <lylesimmons125@gmail.com>
2024-02-27 09:21:16 -0500

    challenge complete

--- javascript/find_first_duplicate.js
+++ javascript/find_first_duplicate.js
@@ -1,5 +1,14 @@
 function findFirstDuplicate(arr) {
   // type your code here
+  const uniques = new Set()
+  for (const value of arr) {
+    if (uniques.has(value)) {
+      return value
+    } 
+    uniques.add(value)
+  }
+
+  return -1
 }
 
 if (require.main === module) {
@@ -17,3 +26,18 @@ module.exports = findFirstDuplicate;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+
+/*
+I needed some help with this one. I was able to get to the point where I could get 
+5 out of 6 tests passing using a for loop
+
+I iterated through the array and checked if array at index i was equal to 
+array at index i+1. If no matches return -1
+
+This worked for the majority but if the duplicate was not immediately after the
+current value it would not find the match
+
+looking at the solution I learned of the set() method and how to use this which is helpful
+
+
+*/
